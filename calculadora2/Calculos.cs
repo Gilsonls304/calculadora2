d9bcc7710e71c9706b804ab97eeaee0b5ac07c08
Gilson Luiz De Souza <gilson.souza@squadra.com.br>
2019-04-24 14:57:53 -0300

    Add project files.

---
+++ calculadora2/Calculos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora2
+{
+	class Calculos
+	{
+		public decimal total_investimento { get; set; }
+		public decimal totalfinal { get; set; }
+		public decimal totalrenda_fixa { get; set; }
+
+		public decimal impostorenda { get; set; }//25%-ate 12 meses; 15% de 13 a 24 meses; 5% de 25% a 36 meses; 1% acima de 36 meses
+		public void calcula(int meses, decimal valor, decimal rendimento, decimal rendafixa)
+		{
+			total_investimento = valor * (rendimento / 100) * meses;
+			totalfinal = valor + total_investimento;
+			totalrenda_fixa = valor * (rendafixa / 100) * meses;
+
+			if (meses <= 12)
+			{
+				impostorenda = totalrenda_fixa - totalrenda_fixa * 25 / 100;
+			}
+			else if (meses > 12 && meses < 25)
+			{
+				impostorenda = totalrenda_fixa - totalrenda_fixa * 15 / 100;
+			}
+			else if (meses > 24 && meses < 37)
+			{
+				impostorenda = totalrenda_fixa - totalrenda_fixa * 5 / 100;
+			}
+			else if (meses > 36)
+			{
+				impostorenda = totalrenda_fixa - totalrenda_fixa * 1 / 100;
+			}
+		}
+	}
+}
---
+++ calculadora2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			decimal valor_aplicado = 0;
+			decimal rendimento = 0;
+			decimal renda_fixa = 0;
+			int quant_meses = 0;
+			Boolean i = false;
+
+			Calculos calculo = new Calculos();//Instanciando a classe que possui o método 
+											  //que calcula os rendimentos e imposto de renda
+
+			while (i == false)//Repete a solicitação dos dados para calculo de rendimento caso 
+							  //sejam digitados valores inválidos como 0 e valores negativos
+			{
+				Console.WriteLine("Digite o valor a ser aplicado (R$):\n ");
+				decimal.TryParse(Console.ReadLine(), out valor_aplicado);
+				Console.WriteLine();
+				Console.WriteLine("Digite o rendimento mensal desejado, modalidade Poupança (%):\n ");
+				decimal.TryParse(Console.ReadLine(), out rendimento);
+				Console.WriteLine("Digite a quantidade de meses que o dinheiro ficará aplicado:\n ");
+				Console.WriteLine();
+				int.TryParse(Console.ReadLine(), out quant_meses);
+				Console.WriteLine();
+				Console.WriteLine("Digite o redimento (%) desejado para renda fixa:\n ");
+				decimal.TryParse(Console.ReadLine(), out renda_fixa);
+
+				if (valor_aplicado > 0 && rendimento > 0 && renda_fixa > 0 && quant_meses > 0)
+				{
+
+					calculo.calcula(quant_meses, valor_aplicado, rendimento, renda_fixa);// chama o método que calcula o rendimento
+
+					Console.WriteLine(valor_aplicado + " Reais aplicado na Poupança a um rendimento mensal de " + rendimento + "%, durante "
+				+ quant_meses + " meses, rende um total de " + calculo.total_investimento + " Reais \n");
+					if (quant_meses <= 12)
+					{
+						Console.WriteLine(valor_aplicado + " Reais aplicado a uma renda fixa com investimento de " + renda_fixa + "%, durante "
+						+ quant_meses + " meses, imposto de renda de 25%, rende um total de " + calculo.impostorenda + " Reais \n");
+					}
+					else if (quant_meses > 12 && quant_meses < 25)
+					{
+						Console.WriteLine(valor_aplicado + " Reais aplicado a uma renda fixa com investimento de " + renda_fixa + "%, durante "
+						+ quant_meses + " meses, imposto de renda de 15%, rende um total de " + calculo.impostorenda + " Reais \n");
+
+					}
+					else if (quant_meses > 24 && quant_meses < 37)
+					{
+						Console.WriteLine(valor_aplicado + " Reais aplicado a uma renda fixa com investimento de " + renda_fixa + "%, durante "
+						+ quant_meses + " meses, imposto de renda de 5%, rende um total de " + calculo.impostorenda + " Reais \n");
+					}
+					else if (quant_meses > 36)
+					{
+						Console.WriteLine(valor_aplicado + " Reais aplicado a uma renda fixa com investimento de " + renda_fixa + "%, durante "
+						+ quant_meses + " meses, imposto de renda de 1%, rende um total de " + calculo.impostorenda + " Reais \n");
+					}
+
+					if (calculo.total_investimento == calculo.impostorenda)
+					{
+						Console.WriteLine("As duas modalidades de aplicação geram o mesmo retorno de investimento!");
+					}
+					else if (calculo.total_investimento > calculo.impostorenda)
+					{
+						Console.WriteLine("A modalidade de aplicação Poupança é mais vantajosa, pois o rendimento de: " +
+							calculo.total_investimento + " é maior que " + calculo.impostorenda);
+					}
+					else
+					{
+						Console.WriteLine("A modalidade de aplicação renda fixa é mais vantajosa, pois o rendimento de: " +
+							calculo.impostorenda + " é maior que " + calculo.total_investimento);
+						Console.WriteLine("renda fixa: " + calculo.totalrenda_fixa);
+					}
+
+					i = true;
+
+					Console.Read();
+
+				}
+				else
+				{
+					Console.WriteLine("Valores negativos ou iguais a zero não são válido, por favor digite novamente \n");
+					i = false;
+				}
+			}
+		}
+	}
+}
